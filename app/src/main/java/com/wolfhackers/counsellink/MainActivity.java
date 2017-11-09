5c29a97aa2dfaa0535f991021cf105dbff2dbd94
Omkar Joshi <onjoshi@ncsu.edu>
2017-11-09 18:04:38 -0500

    Added calendarview and progressbar

--- app/src/main/java/com/wolfhackers/counsellink/MainActivity.java
+++ app/src/main/java/com/wolfhackers/counsellink/MainActivity.java
@@ -7,19 +7,43 @@ import android.app.PendingIntent;
 import android.app.NotificationManager;
 import android.support.v4.app.NotificationCompat;
 import android.app.Notification;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
 import com.wolfhackers.counsellink.ExpandClass;
 import com.wolfhackers.counsellink.RejectClass;
 import com.wolfhackers.counsellink.R;
+
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private static AsyncHttpClient client = new AsyncHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("key", "value");
+        params.put("more", "data");
+        client.get("http://www.google.com", params, new TextHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, String res) {
+                        // called when response HTTP status is "200 OK"
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String res, Throwable t) {
+                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                    }
+                }
+        );
     }
 
 
